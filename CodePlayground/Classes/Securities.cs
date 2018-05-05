ccd52c6e828faf361733c7c0293cff64bc7ea4f7
Joe Ashbrook <jashbrook@toddinvestment.com>
2018-05-05 09:01:15 -0400

    Add project files.

---
+++ CodePlayground/Classes/Securities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePlayground
+{
+    /// <summary>
+    ///Securities class w/ constuctor 
+    /// </summary>
+    public class Securities
+    {
+        public Securities()
+        {
+
+        }
+        public Securities(string symbol, string name, double marketCap, int epsRating, int rsRating, string adRating)
+        {
+            Symbol = symbol;
+            Name = name;
+            MarketCap = marketCap;
+            EPS_Rating = epsRating;
+            RS_Rating = rsRating;
+            AD_Rating = adRating;
+        }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public double MarketCap { get; set; }
+        public int EPS_Rating { get; set; }
+        public int RS_Rating { get; set; }
+        public string AD_Rating { get; set; }
+    }
+    /// <summary>
+    ///  Repository Pattern
+    /// </summary>
+    public static class SecurityRepository
+    {
+        public static List<Securities> LoadSecurities()
+        {
+            return new List<Securities>
+            {
+                new Securities ("ITT", "ITT Inc", 32741.1, 88, 97, "A-"),
+                new Securities ("A", "Agilent Technologies Inc", 21630.1, 68, 87, "B-"),
+                new Securities ("CEL", "Cellcom Israel Ltd", 973.9, 32, 47, "B+"),
+                new Securities ("AA", "Alcoa Corporation", 8783.8, 79, 95, "B" ),
+                new Securities ("AAAP", "Advanced Acce Applic Ads", 3117.2, 1, 98, "A+" ),
+                new Securities ("TEAM", "Atlassian Corp Plc Cl A", 10686.6, 96, 95, "A-")
+            };
+        }
+    }
+}
---
+++ CodePlayground/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program codePlayground = new Program();
+            List<Securities> securityList = SecurityRepository.LoadSecurities();
+
+            //Securities filterdSecurities = codePlayground.QuerySymbol(securityList);
+            //codePlayground.ReadOutSecurity(filterdSecurities);
+
+            IEnumerable<Securities> securitiesByMarketCap = codePlayground.QueryMarketCapBySize(securityList);
+            codePlayground.ReadOutSecuritiesMarketCap(securitiesByMarketCap);
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Convert C# anonymous type into a explicit type.
+        /// The var keyword is used to hold anonymous collections.
+        /// </summary>
+        /// <param name="securityList"></param>
+        private Securities QuerySymbol(List<Securities> securityList)
+        {
+            Securities securities = new Securities();
+
+            var anonymousType = from s in securityList
+                        where s.Symbol.Equals("A")
+                        select new
+                        {
+                            s.Symbol,
+                            s.Name,
+                            s.MarketCap,
+                            s.EPS_Rating,
+                            s.RS_Rating,
+                            s.AD_Rating
+                        };
+
+            foreach (var item in anonymousType)
+            {
+                securities = new Securities(item.Symbol, item.Name, item.MarketCap, item.EPS_Rating, item.RS_Rating, item.AD_Rating);
+            }
+            return securities;
+        }
+        /// <summary>
+        /// Store ordered securities directly in an IEnumerable of type Securities.
+        /// Note that IEnumerable describes behavior while List is an implementation of that behavior.
+        /// </summary>
+        /// <param name="securityList"></param>
+        /// <returns></returns>
+        private IEnumerable<Securities> QueryMarketCapBySize(List<Securities> securityList)
+        {
+            IEnumerable<Securities> sortedMarketCap = from security in securityList
+                                                      orderby security.MarketCap descending
+                                                      select security;
+
+            return sortedMarketCap;
+        }
+
+        private void ReadOutSecurity(Securities securities)
+        {
+            Console.WriteLine(securities.Name);
+        }
+
+        private void ReadOutSecuritiesMarketCap(IEnumerable<Securities> securityList)
+        {
+            Console.WriteLine("---Securities By MarketCap Descending---");
+            foreach (Securities security in securityList)
+            {
+                Console.WriteLine(security.Name + ": " + security.MarketCap.ToString("C"));
+            }
+        }
+
+        static void LoopOverSecurities(List<Securities> securityList)
+        {
+            Console.WriteLine("Total Securities: " + securityList.Count());
+            Console.WriteLine("========================================");
+            foreach (Securities security in securityList)
+            {
+                Console.WriteLine(security.Name);
+            }
+        }
+    }
+}
